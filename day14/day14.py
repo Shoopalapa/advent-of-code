cbd2e3c4150fe2fba4cc8de65502433e38692c48
Wyatt Prill <wyattprill@gmail.com>
2020-12-14 23:05:36 -0500

    Finished pt 2, will condense later

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,7 @@
+import pprint
+
 def step1(lines):
-	mem = [0] * 99999
+	mem = [0] * 9999
 
 	for instructions in lines:
 		if instructions[:4] == "mask":
@@ -12,33 +14,73 @@ def step1(lines):
 	return(sum(mem))
 
 def step2(lines):
-	mem = [0] * 99999
+	mem = {}
 
 	for instructions in lines:
 		if instructions[:4] == "mask":
 			mask = instructions.split(" = ")[1]
 		else:
-			index = int(instructions.split("] = ")[0][4:])
-			indexes = indexScrambler(index,mask)
+			indexBit = int2bit(int(instructions.split("] = ")[0][4:]),36)
+			indexes = indexScrambler(indexBit,mask)
 
 			value = int(instructions.split(" = ")[1])
-			tempVal = bitmask(value,mask)
 
 			for tempIndex in indexes:
-				mem[tempIndex] = tempVal
+				mem[tempIndex] = value
 
-	return(sum(mem))
 
+	return(sum(mem.values()))
+
+def indexScrambler(inputBit, mask):
+	indexMask = ""
+
+	for val, mas in zip(list(inputBit), list(mask)):
+		if mas == "0":
+			indexMask += val
+		elif mas == "1":
+			indexMask += "1"
+		else:
+			indexMask += "X"
+
+	permList = []
+	for perms in range(2**(indexMask.count("X"))):
+		permList.append(int2bit(perms,indexMask.count("X")))
+
+	indexList = [indexMask] * 2**(indexMask.count("X"))
+	result = []
+	for idx, perm in zip(indexList,permList):
+		temp = list(idx)
+		temp2 = list(perm)
+		for i in range(temp.count("X")):
+			temp[temp.index("X")] = temp2[0]
+			temp2 = temp2[1:]
+		result.append("".join(temp))
+
+	for i in range(len(result)):
+		result[i] = bit2int(result[i])
+	return(result)
+
+def int2bit(inputInt,size):
+	result = ""
+	for i in range(size):
+		if 2**(size-1-i) <= inputInt:
+			inputInt -= 2**(size-1-i)
+			result += "1"
+		else:
+			result += "0"
+	return(result)
+
+def bit2int(inputBit):
+	result = 0
+	for index in range(36):
+		if inputBit[index] == "1":
+			result += 2**(35-index)
+
+	return(result)
 
 def bitmask(value,mask):
-	valueInt = int(value)
-	valueBit = ""
-	for i in range(36):
-		if 2**(35-i) <= valueInt:
-			valueInt -= 2**(35-i)
-			valueBit = valueBit + "1"
-		else:
-			valueBit = valueBit + "0"
+
+	valueBit = int2bit(value,36)
 
 	resultBit = ""
 	for val, mas in zip(list(valueBit),list(mask)):
@@ -47,16 +89,12 @@ def bitmask(value,mask):
 		else:
 			resultBit = resultBit + val
 
-	resultInt = 0
-	for bit in range(36):
-		if resultBit[bit] == "1":
-			resultInt += 2**(35-bit)
-
-	return(resultInt)
+	return(bit2int(resultBit))
 
 def main():
 	with open("input.txt") as inputfile:
 		lines = inputfile.read().splitlines()
-		print(step1(lines))
+		#print(step1(lines))
+		print(step2(lines))
 
 main()
