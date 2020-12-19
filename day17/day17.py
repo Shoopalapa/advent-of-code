8733105f5ebe4d3348ca61cb894ca3c10dbf3a3c
Wyatt Prill <wyattprill@gmail.com>
2020-12-19 17:24:19 -0500

    3d conway is making me sad

--- day17/day17.py
+++ day17/day17.py
@@ -2,11 +2,34 @@ import copy
 import pprint
 
 def step1(lines,steps):
-	space = ([["."*len(lines[0])] * len(lines)] * steps) + [lines] + ([["."*len(lines[0])]*len(lines)]*steps)
+	space = ([[["."]*len(lines[0])] * len(lines)] * steps) + [lines] + ([[["."]*len(lines[0])]*len(lines)]*steps)
 	currentSpace = copy.deepcopy(space)
 
-	for itr in range(steps):
-		pass
+	pprint.pprint(space[0])
+	# for itr in range(steps):
+	# 	print("Cycle #"+str(itr+1))
+
+	# 	for z in range(steps+1):
+	# 		print("Z="+str(z))
+
+	# 		for y in range(len(lines)):
+	# 			for x in range(len(lines[0])):
+
+	# 				count = 0
+	# 				for neighbor in returnNeighbors(x,y,z,len(lines)-1,steps,1):
+	# 					if currentSpace[neighbor[0]][neighbor[1]][neighbor[2]] == "#":
+	# 						count += 1
+
+	# 				if space[x][y][z] == "#" and not(count == 2 or count == 3):
+	# 					currentSpace[x][y][z] = "."
+	# 				elif space[x][y][z] == "." and count == 3:
+	# 					currentSpace[x][y][z] == "#"
+	# 				else:
+	# 					currentSpace[x][y][z] == space[x][y][z]
+
+	# 		pprint.pprint(currentSpace[x][y])
+	# 	space = copy.deepcopy(currentSpace)
+
 	return(space)
 
 def returnNeighbors(x,y,z,boundaryXY,boundaryZ,distance=1,):
@@ -17,7 +40,7 @@ def returnNeighbors(x,y,z,boundaryXY,boundaryZ,distance=1,):
 				diffX = x+a
 				diffY = y+b
 				diffZ = z+c
-				if not (diffX<0 or diffY<0 or diffZ<0 or diffX>boundaryXY or diffY>boundaryXY or diffZ>boundaryZ or a,b,c==0,0,0):
+				if not (diffX<0 or diffY<0 or diffZ<0 or diffX>boundaryXY or diffY>boundaryXY or diffZ>boundaryZ):
 					neighbors.append((x+a,y+b,z+c))
 
 	return(neighbors)
@@ -30,5 +53,5 @@ def main():
 		lines = inputfile.read().splitlines()
 		pprint.pprint(step1(lines,1))
 
-#main()
-print(returnNeighbors(7,7,7,9,6,1))
+main()
+#print(returnNeighbors(0,0,6,9,6,1))
